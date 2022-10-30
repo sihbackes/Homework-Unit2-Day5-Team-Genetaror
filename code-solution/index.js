c4f7d17eeb0b1c37529fc5fe20b4a9429c943011
Simone Backes <88430766+sihbackes@users.noreply.github.com>
2022-10-30 22:17:01 +0000

    reset and teams

---
+++ code-solution/index.js
@@ -0,0 +1,77 @@
+let textInputNode = document.getElementById("name-input");
+let nameFieldNode = document.querySelector(".name-grid");
+let nameUlNode = document.querySelector(".names-list");
+let divTemasNode = document.querySelector(".teams-generated");
+let numberValueNode = document.getElementById("number-value");
+
+let names = [];
+let numberOfTeams = 1;
+const decrementTeams = () => {
+  if (numberOfTeams > 1) {
+    numberOfTeams--;
+    numberValueNode.innerText = numberOfTeams;
+  }
+};
+
+const incrementTeams = () => {
+  numberOfTeams++;
+  numberValueNode.innerText = numberOfTeams;
+};
+
+const addName = () => {
+  let nameValue = textInputNode.value;
+  if (nameValue.length === 0) {
+    return;
+  }
+  names.push(nameValue);
+  liNode = document.createElement("li");
+  nameUlNode.appendChild(liNode);
+  liNode.innerText = nameValue;
+  textInputNode.value = "";
+};
+
+const resetTeams = () => {
+  names = [];
+  nameFieldNode.innerHTML = "";
+};
+
+let generateTeams = () => {
+  let teamNodes = [];
+  divTemasNode.innerHTML = "";
+  for (let i = 0; i < numberOfTeams; i++) {
+    let h4Node = document.createElement("h4");
+    let teamContainerNode = document.createElement("div");
+    teamContainerNode.classList.add("card");
+    h4Node.innerHTML = `Team ${i + 1}`;
+    teamContainerNode.appendChild(h4Node);
+    divTemasNode.appendChild(teamContainerNode);
+    let ulNode = document.createElement("ul");
+    teamContainerNode.classList.add("team");
+    teamNodes.push(ulNode);
+    teamContainerNode.appendChild(ulNode);
+  }
+  // store the number of names because when we remove a name the length will change
+  let nameLength = names.length;
+  for (let i = 0; i < nameLength; i++) {
+    // choose a random index
+    let randomIndex = Math.floor(Math.random() * names.length);
+    let liNode = document.createElement("li");
+    liNode.innerText = names[randomIndex];
+    // choose teams one after the other
+    // example with 3 teams
+    // i          = 0 1 2 3 4 5 6 7 8 9
+    // teamIndex  = 0 1 2 0 1 2 0 1 2 0
+    let teamIndex = i % numberOfTeams;
+    teamNodes[teamIndex].appendChild(liNode);
+    // remove the selected name from the array
+    names.splice(randomIndex, 1);
+  }
+};
+
+window.onload = () => {
+  textInputNode.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      addName();
+    }
+  });
+};
--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let nameTeam = document.getElementById("input-add-member");
 let display = document.getElementById("list-display-names");
 let teams = document.getElementById("input-add-teams");
 let containerTitle = document.getElementById("container-title");
+let box = document.getElementById("box-display-names");
+let teamsBox = document.getElementById("container-teams");
 let listOfNames = [];
 let newList = [];
+
 function addTeamMember() {
   listOfNames.push(nameTeam.value); // get the name on the input and push to array listOfNames
   let nameList = document.createElement("li"); // create an item li to display the name
@@ -15,18 +18,9 @@ function addTeamMember() {
 
 number = Number(teams.value); //get the number of how many teams we want
 
-function randomTeams() {
-  let arrayOfTeams = [];
-  let average = newList.length / number;
-  for (let i = 0; i < newList.length; i += average) {
-    arrayOfTeams.push(newList.slice(i, i + average));
-    console.log(arrayOfTeams[i]);
-  }
-  return arrayOfTeams;
-}
-
 function displayTeams() {
   number = Number(teams.value); //get the number of how many teams we want
+  let arrayOfTeams = [];
 
   for (let i = 1; i <= number; i++) {
     let container = document.createElement("div"); //creates a new div
@@ -38,5 +32,34 @@ function displayTeams() {
     let newBoxTeam = document.createElement("div"); //create a new div to display the names
     newBoxTeam.classList.add("box-display-teams"); //add the style class
     container.appendChild(newBoxTeam); //put the display on the location
+    let teamsList = document.createElement("ol");
+    teamsList.setAttribute("id", i);
+    newBoxTeam.appendChild(teamsList);
+  }
+
+  let average = newList.length / number;
+  for (let i = 0; i < newList.length; i += average) {
+    arrayOfTeams.push(newList.slice(i, i + average));
+  }
+  for (let index = 0; index < arrayOfTeams.length; index++) {
+    for (let i = 0; i < arrayOfTeams[index].length; i++) {
+      let namesOnList = document.createElement("li");
+      let olfind = document.getElementById(index + 1);
+
+      olfind.appendChild(namesOnList);
+      namesOnList.innerText = arrayOfTeams[index][i];
+    }
   }
 }
+function resetTeams() {
+  box.innerText = "";
+  teamsBox.innerText = "";
+}
+
+window.onload = () => {
+  nameTeam.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      addTeamMember();
+    }
+  });
+};
